a994a0581a4760bdcf62e4239da858cde4590611
Martyna Chojnowska <martynachojnowska12@gmail.com>
2023-11-28 09:38:35 +0100

    Added response about chance of rain and sno

--- mixins.py
+++ mixins.py
@@ -12,15 +12,24 @@ class ConnectionMixin:
         response = requests.get(url)
         return response
 
+    def connect_with_api_forecast(self, city):
+        url = self.url + '/forecast.json?' + f'key={self.api_key}' + '&' + f'q={city}'
+        response = requests.get(url)
+        return response
+
     def load_data_from_api_to_dict(self, city):
         data = self.connect_with_api(city).json()
+        forecast_data = self.connect_with_api_forecast(city).json()
+        print(data)
+        print(forecast_data)
         weather_data = {
-            "temp": data.get('temp_c'),
-            "humidity": data.get('humidity'),
-            "cloud": data.get('cloud'),
-            "wind": data.get('wind_kph'),
-            "pressure": data.get('pressure_mb'),
-            "snow": data.get('daily_chance_of_snow'),
-            "rain": data.get('daily_chance_of_rain')
+            "name": data['location']['name'],
+            "temp": data['current']['temp_c'],
+            "humidity": data['current']['humidity'],
+            "cloud": data['current']['cloud'],
+            "wind": data['current']['wind_kph'],
+            "pressure": data['current']['pressure_mb'],
+            "rain": forecast_data['forecast']['forecastday'][0]['day']['daily_chance_of_rain'],
+            "snow": forecast_data['forecast']['forecastday'][0]['day']['daily_chance_of_snow'],
         }
         return weather_data
